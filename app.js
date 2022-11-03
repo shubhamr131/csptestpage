6064a105de82c16878b7977e4debfb652fe24fa0
shubhamr131 <shubham.rauniyar@acquire.io>
2022-11-03 16:14:59 +0545

    Added cobrowse on index.page

--- app.js
+++ app.js
@@ -1,20 +1,17 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const helmet = require('helmet');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+
 
 var app = express();
+app.use(helmet.frameguard({ action: 'SAMEORIGIN' }));
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
 module.exports = app;
